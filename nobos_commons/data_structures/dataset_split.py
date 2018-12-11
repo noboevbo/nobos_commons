ec91f44102492801a0bdf219d5c533178691df2d
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2018-12-11 12:47:56 +0100

    DatasetSplit serializable

--- nobos_commons/data_structures/dataset_split.py
+++ nobos_commons/data_structures/dataset_split.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from nobos_commons.data_structures.constants.dataset_slit_type import DatasetSplitType
 
 
@@ -7,3 +9,14 @@ class DatasetSplit(object):
     def __init__(self, dataset_name: str, dataset_split_type: DatasetSplitType):
         self.dataset_name = dataset_name
         self.dataset_split_type = dataset_split_type
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'dataset_name': self.dataset_name,
+            'dataset_split_type': self.dataset_split_type
+        }
+
+    @staticmethod
+    def from_dict(dict_in: Dict[str, Any]) -> 'DatasetSplit':
+        return DatasetSplit(dataset_name=dict_in['dataset_name'],
+                            dataset_split_type=dict_in['dataset_split_type'])
