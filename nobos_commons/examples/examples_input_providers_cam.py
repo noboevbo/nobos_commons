75b448e484179bab0e40a7710d9d21efb6c536e6
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-01-07 06:07:15 +0100

    Updated examples for input providers

--- nobos_commons/examples/examples_input_providers_cam.py
+++ nobos_commons/examples/examples_input_providers_cam.py
@@ -1,21 +1,27 @@
 import cv2
 
+from nobos_commons.data_structures.dimension import ImageSize
 from nobos_commons.input_providers.camera.img_dir_provider import ImgDirProvider
 from nobos_commons.input_providers.camera.webcam_provider import WebcamProvider
 from nobos_commons.tools.fps_tracker import FPSTracker
 
 
 def webcam_example():
-    provider = WebcamProvider()
+    provider = WebcamProvider(fps=60, image_size=ImageSize(1280, 720), camera_number=0)
     fps_tracker = FPSTracker()
-    for i in provider.get_frame():
+    for i in provider.get_data():
         cv2.imshow('webcam', i)
         fps_tracker.print_fps()
 
 
-def img_dir_example():
-    provider = ImgDirProvider("/home/dennis/Downloads/tmp/out", fps=120)
+def img_dir_example(img_dir: str):
+    provider = ImgDirProvider(img_dir, fps=120)
     fps_tracker = FPSTracker(average_over_seconds=2)
-    for i in provider.get_frame():
+    for i in provider.get_data():
         cv2.imshow('img_dir', i)
         fps_tracker.print_fps()
+
+
+if __name__ == "__main__":
+    webcam_example()
+    # img_dir_example("/media/disks/beta/datasets/ofp_tests/ofp_parkplatz_walking_02/FrontCamera")
