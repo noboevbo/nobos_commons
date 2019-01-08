f6093f586280a6bc6bb68bff1d68337d76852be7
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-01-08 13:42:09 +0100

    Export skeleton config, not only limbs

--- nobos_commons/data_structures/skeletons/skeleton_base.py
+++ nobos_commons/data_structures/skeletons/skeleton_base.py
@@ -32,15 +32,22 @@ class SkeletonBase(object):
     def copy_from_dict(self, in_dict: Dict[str, Any]):
         self.joints.copy_from_dict(in_dict['joints'])
 
-    def get_limb_config_dict(self) -> Dict[str, Dict[str, Any]]:
-        limb_dict: Dict[str, Dict[str, Any]] = {}
+    def get_skeleton_config_dict(self) -> Dict[str, Dict[str, Any]]:
+        limb_list = []
+        joint_list = []
         for limb in self.limbs:
             limb_color = None
             if self.limb_colors[limb.num] is not None:
                 limb_color = self.limb_colors[limb.num].hex
-            limb_dict[limb.name] = {
-                "color": limb_color,
-                "joint_from": limb.joint_from.to_dict(),
-                "joint_to": limb.joint_to.to_dict()
-            }
-        return limb_dict
+            limb_dict = limb.to_dict()
+            limb_dict["color"] = limb_color
+            limb_list.append(limb_dict)
+        for joint in self.joints:
+            joint_color = None
+            if self.joint_colors[joint.num] is not None:
+                joint_color = self.joint_colors[joint.num].hex
+            joint_dict = joint.to_dict()
+            joint_dict["color"] = joint_color
+            joint_list.append(joint_dict)
+        skeleton_config: Dict[str, Dict[str, Any]] = {"limbs": limb_list, "joints": joint_list}
+        return skeleton_config
