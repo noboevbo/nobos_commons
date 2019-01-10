be995f5baacf67051ffd2cf85bbe8a5ea5857d80
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-01-10 14:41:44 +0100

    Added image size an dloop setting to imgdirprovider

--- nobos_commons/input_providers/camera/img_dir_provider.py
+++ nobos_commons/input_providers/camera/img_dir_provider.py
@@ -4,6 +4,7 @@ import time
 import cv2
 import numpy as np
 
+from nobos_commons.data_structures.dimension import ImageSize
 from nobos_commons.input_providers.input_provider_base import InputProviderBase
 from nobos_commons.utils.file_helper import get_img_paths_from_folder
 
@@ -11,7 +12,9 @@ from nobos_commons.utils.file_helper import get_img_paths_from_folder
 class ImgDirProvider(InputProviderBase):
     def __init__(self,
                  img_dir: str,
-                 fps: int = None):
+                 fps: int = None,
+                 loop: bool = False,
+                 image_size: ImageSize = None):
         """
         Provides images from a given image directory.
         :param img_dir: The directory which contains the images
@@ -21,19 +24,25 @@ class ImgDirProvider(InputProviderBase):
         assert os.path.exists(img_dir), 'Image directory not found!'
         self.img_paths = sorted(get_img_paths_from_folder(img_dir))
         self.fps = fps
+        self.loop = loop
+        self.image_size = image_size
 
     def get_data(self) -> np.ndarray:
         assert len(self.img_paths) > 0, 'No images found'
+        while True:
+            for img_path in self.img_paths:
+                if self.fps is not None:
+                    start = time.time()
+                img = cv2.imread(img_path)
+                if self.image_size is not None:
+                    img = cv2.resize(img, (self.image_size.width, self.image_size.height))
+                yield img
 
-        for img_path in self.img_paths:
-            if self.fps is not None:
-                start = time.time()
-            img = cv2.imread(img_path)
-            yield img
+                key = cv2.waitKey(1)
+                if key & 0xFF == ord('q'):
+                    return None
 
-            key = cv2.waitKey(1)
-            if key & 0xFF == ord('q'):
-                return None
-
-            if self.fps is not None:
-                time.sleep(max(1. / self.fps - (time.time() - start), 0))
+                if self.fps is not None:
+                    time.sleep(max(1. / self.fps - (time.time() - start), 0))
+            if not self.loop:
+                break
