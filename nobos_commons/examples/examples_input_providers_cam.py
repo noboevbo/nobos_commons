d7f53d54dee0b3723a44b91a17d45c90f00023ab
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-01-10 10:38:34 +0100

    Added webcam provider async

--- nobos_commons/examples/examples_input_providers_cam.py
+++ nobos_commons/examples/examples_input_providers_cam.py
@@ -2,7 +2,7 @@ import cv2
 
 from nobos_commons.data_structures.dimension import ImageSize
 from nobos_commons.input_providers.camera.img_dir_provider import ImgDirProvider
-from nobos_commons.input_providers.camera.webcam_provider import WebcamProvider
+from nobos_commons.input_providers.camera.webcam_provider import WebcamProvider, WebcamProviderAsync
 from nobos_commons.tools.fps_tracker import FPSTracker
 
 
@@ -13,6 +13,12 @@ def webcam_example():
         cv2.imshow('webcam', i)
         fps_tracker.print_fps()
 
+def webcam_example_async():
+    provider = WebcamProviderAsync(fps=60, image_size=ImageSize(1280, 720), camera_number=0)
+    fps_tracker = FPSTracker()
+    for i in provider.get_data():
+        cv2.imshow('webcam', i)
+        fps_tracker.print_fps()
 
 def img_dir_example(img_dir: str):
     provider = ImgDirProvider(img_dir, fps=120)
@@ -23,5 +29,5 @@ def img_dir_example(img_dir: str):
 
 
 if __name__ == "__main__":
-    webcam_example()
+    webcam_example_async()
     # img_dir_example("/media/disks/beta/datasets/ofp_tests/ofp_parkplatz_walking_02/FrontCamera")
--- nobos_commons/input_providers/camera/webcam_provider.py
+++ nobos_commons/input_providers/camera/webcam_provider.py
@@ -1,3 +1,6 @@
+from queue import Queue
+from threading import Thread
+
 import cv2
 import numpy as np
 
@@ -42,4 +45,69 @@ class WebcamProvider(InputProviderBase):
 
     def stop(self):
         self.cap.release()
+
+
+class WebcamProviderAsync(InputProviderBase):
+    def __init__(self,
+                 camera_number: int = 0,
+                 image_size: ImageSize = ImageSize(width=1280, height=720),
+                 fps: int = 60,
+                 queue_size = 128):
+        """
+        Provides frames captured from a webcam. Uses OpenCV internally.
+        :param camera_number: The cameras id
+        :param image_size: The image size which should be used, may be limited by camera parameters
+        :param fps: The fps on which the frames should be grabbed, may be limited by camera parameters
+        """
+        self.cap = cv2.VideoCapture(camera_number)
+        self.stopped = True
+        self.queue = Queue(maxsize=queue_size)
+        self.image_size = image_size
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.image_size.width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.image_size.height)
+        self.cap.set(cv2.CAP_PROP_FPS, fps)
+
+    def start(self):
+        self.stopped = False
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        assert self.cap.isOpened(), 'Cannot capture source'
+
+        while self.cap.isOpened():
+            if self.stopped:
+                return None
+            if not self.queue.full():
+                ret, frame = self.cap.read()
+                if ret:
+                    self.queue.put(frame)
+
+                    key = cv2.waitKey(1)
+                    if key & 0xFF == ord('q'):
+                        self.stop()
+                        return None
+
+                else:
+                    self.stop()
+                    return None
+
+        self.cap.release()
+
+    def get_data(self) -> np.ndarray:
+        assert self.cap.isOpened(), 'Cannot capture source'
+        if self.stopped:
+            self.start()
+        while True:
+            if self.stopped:
+                return None
+            if self.queue.qsize() > 0:
+                yield self.queue.get()
+
+    def stop(self):
+        self.stopped = True
+        self.cap.release()
+
 
