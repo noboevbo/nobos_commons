322c75a893bfc4cc830a070e77711a0878c59143
Dennis Ludl <Dennis.Ludl@reutlingen-university.de>
2019-01-16 09:03:23 +0100

    Added skeleton converter openpose to stickman

---
+++ nobos_commons/tools/skeleton_converters/skeleton_converter_openpose_to_stickman.py
@@ -0,0 +1,28 @@
+from nobos_commons.data_structures.skeletons.joint_2d import Joint2D
+from nobos_commons.data_structures.skeletons.skeleton_openpose import SkeletonOpenPose
+from nobos_commons.data_structures.skeletons.skeleton_stickman import SkeletonStickman
+from nobos_commons.tools.skeleton_converters.skeleton_converter_base import SkeletonConverter
+from nobos_commons.utils.joint_helper import get_middle_joint
+
+
+class SkeletonConverterOpenPoseToStickman(SkeletonConverter):
+    def get_converted_skeleton(self, skeleton_coco: SkeletonOpenPose) -> SkeletonStickman:
+        skeleton_stickman: SkeletonStickman = self._get_skeleton_from_joints(skeleton_coco)
+        self._set_calculated_joints(skeleton_stickman)
+        return skeleton_stickman
+
+    # Private methods
+
+    def _get_skeleton_from_joints(self, skeleton_coco: SkeletonOpenPose) -> SkeletonStickman:
+        skeleton_stickman: SkeletonStickman = SkeletonStickman()
+        for joint in skeleton_coco.joints:
+            if joint.name in skeleton_stickman.joints:
+                skeleton_stickman.joints[joint.name].copy_from(joint, allow_different_num=True)
+        return skeleton_stickman
+
+    def _set_calculated_joints(self, skeleton_stickman: SkeletonStickman):
+        calculated_hip_center: Joint2D = get_middle_joint(joint_a=skeleton_stickman.joints.left_hip,
+                                                          joint_b=skeleton_stickman.joints.right_hip)
+
+        if calculated_hip_center is not None:
+            skeleton_stickman.joints.hip_center.copy_from(calculated_hip_center)
