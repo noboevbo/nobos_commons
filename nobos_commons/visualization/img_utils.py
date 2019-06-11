a6c320c2a93483366f497fcc3e2d9ae661941137
Dennis Name <Dennis.Ludl@reutlingen-university.de>
2019-06-11 16:52:41 +0200

    Image title CV2 -> PIL

--- nobos_commons/visualization/img_utils.py
+++ nobos_commons/visualization/img_utils.py
@@ -1,11 +1,27 @@
 from typing import List
 
 import cv2
+import numpy as np
+from PIL import ImageFont, Image, ImageDraw
 
 
-def add_img_title(img, title, font_scale: int =1):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, title, (0, 30), font, font_scale, (0, 0, 255), 1, cv2.LINE_AA)
+def add_img_title(img, title, font_size: int = 18):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('Roboto-Bold.ttf', size=font_size)
+    # get text size
+    text_size = font.getsize(title)
+
+    # set button size + 10px margins
+    bg_size = (text_size[0] + 20, text_size[1] + 40)
+    draw.rectangle(((0, 30), bg_size), fill="black")
+    draw.text((10, 30), title, fill=(255, 255, 255), font=font)
+
+    # cv2.putText(img, title, (0, 30), font, font_scale, (0, 0, 255), 1, cv2.LINE_AA)
+    img = np.asarray(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    return img
 
 
 def rgb_to_bgr_color(rgb_color: List[int]):
