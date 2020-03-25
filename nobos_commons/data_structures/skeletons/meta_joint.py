06f07272b551d8784d2ed83ac4f0f7090ee72419
Dennis Name <dennis@noboevbo.de>
2020-03-25 13:04:49 +0100

    (c) forward -> direction

--- nobos_commons/data_structures/skeletons/meta_joint.py
+++ nobos_commons/data_structures/skeletons/meta_joint.py
@@ -9,10 +9,10 @@ from nobos_commons.data_structures.skeletons.joint_visibility import JointVisibi
 
 
 class MetaJoint(object):
-    __slots__ = ['name', 'x', 'y', 'z', 'rotation', 'forward_vec', 'score', 'visibility']
+    __slots__ = ['name', 'x', 'y', 'z', 'rotation', 'direction_vec', 'score', 'visibility']
 
     def __init__(self, num: float = -1, name: str = None, x: float = 0, y: float = 0, z: float = 0, score: float = 0,
-                 visibility: JointVisibility = JointVisibility.VISIBLE, rotation: Quaternion = None, forward_vec: Vec3D = None):
+                 visibility: JointVisibility = JointVisibility.VISIBLE, rotation: Quaternion = None, direction_vec: Vec3D = None):
         """
         Data class for 2D joints
         :param num: The number of the joint in the skeleton configuration
@@ -30,5 +30,5 @@ class MetaJoint(object):
         self.visibility: JointVisibility = visibility
         self.z: float = z
         self.rotation: Quaternion = rotation
-        self.forward_vec: Vec3D = forward_vec
+        self.direction_vec: Vec3D = direction_vec
 
